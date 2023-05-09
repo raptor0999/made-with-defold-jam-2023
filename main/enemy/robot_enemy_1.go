ff16b0465dcafba5250b02495aee13d008a31d80
raptor0999 <raptordev@proton.me>
2023-05-09 16:55:21 -0400

    Fixes enemy collisions with the player and walls and playerfire pushing player out of bounds, etc

--- main/enemy/robot_enemy_1.go
+++ main/enemy/robot_enemy_1.go
@@ -45,6 +45,7 @@ embedded_components {
   "mask: \"player\"\n"
   "mask: \"playerfire\"\n"
   "mask: \"enemy\"\n"
+  "mask: \"walls\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
