8ed5a7486f452d39a2b11bf05448767e370911d4
raptor0999 <raptordev@proton.me>
2023-05-09 18:04:48 -0400

    Adds shadow under player and enemy

--- main/enemy/robot_enemy_1.go
+++ main/enemy/robot_enemy_1.go
@@ -85,3 +85,28 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "sprite1"
+  type: "sprite"
+  data: "tile_set: \"/main/sprites.atlas\"\n"
+  "default_animation: \"circle-shadow\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: -67.0
+    z: 0.09
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 0.061
+    y: 0.123
+    z: 1.0
+  }
+}
