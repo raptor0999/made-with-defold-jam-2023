9193cde7ffe9014f0f956268bf89b952d2f13ee3
raptor0999 <raptordev@proton.me>
2023-05-11 23:35:26 -0400

    Adds new level format and loading. Also adds laser sound.

---
+++ main/player/player_spawner.go
@@ -0,0 +1,34 @@
+components {
+  id: "player_spawner"
+  component: "/main/player/player_spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/main/player/player.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
