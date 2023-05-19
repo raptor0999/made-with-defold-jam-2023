3efc28d26e0edad2acaf8fc66c4932fbfdc78208
raptor0999 <raptordev@proton.me>
2023-05-19 14:43:16 -0400

    Adds player-die sound to game collection so sound plays as player dies and respawns.

--- main/player/player.go
+++ main/player/player.go
@@ -196,26 +196,3 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "player-die"
-  type: "sound"
-  data: "sound: \"/assets/sound/player/player_die.ogg\"\n"
-  "looping: 0\n"
-  "group: \"master\"\n"
-  "gain: 1.0\n"
-  "pan: 0.0\n"
-  "speed: 1.0\n"
-  "loopcount: 0\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
